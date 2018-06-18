c233965a40280e5d31c870c9bae7d2446541c051
Tarek <tareksaier@gmail.com>
2018-06-18 17:41:21 +0900

    working properly with configurable external URL

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ Docker setup for running all parts of the [IIIF Curation Platform](http://codh.r
 * `./setup.sh`: reset everything (application code, configuration, containers)
 * `./reset.sh`: reset containers (i.e application storage, but not configuration)
 * `docker restart <container_id>`: make configuration changes take effect
+
+### Apache proxy config example
+
+        ProxyPassMatch "^/curation/(.*)" "http://127.0.0.1:8001/$1"
+        ProxyPassReverse "^/curation/(.*)" "http://127.0.0.1:8001/$1"
+        ProxyPassMatch "^/ci/(.*)" "http://127.0.0.1:8002/$1"
+        ProxyPassReverse "^/ci/(.*)" "http://127.0.0.1:8002/$1"
+        ProxyPassMatch "^/find/(.*)" "http://127.0.0.1:8003/$1"
+        ProxyPassReverse "^/find/(.*)" "http://127.0.0.1:8003/$1"
+        ProxyPassMatch "^/view/(.*)" "http://127.0.0.1:8004/$1"
+        ProxyPassReverse "^/view/(.*)" "http://127.0.0.1:8004/$1"
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# externalurl=http://127.0.0.1
-#
+externalurl=http://136.187.82.133
+
 # NOTE: services are expected to be accessible at
 # - JSONkeeper: <externalurl>/curation
 # - Canvas Indexer: <externalurl>/ci
@@ -13,8 +13,8 @@
 # - Canvas Indexer: http://127.0.0.1:8002
 # - Curation Finder: http://127.0.0.1:8003
 # - Curation Viewer: http://127.0.0.1:8004
-#
-# exturlexp="${externalurl//\//\\/}"
+
+exturlesc="${externalurl//\//\\/}"
 
 # Jk and CI
 rm -rf JSONkeeper
@@ -23,19 +23,21 @@ git clone https://github.com/IllDepence/JSONkeeper.git
 git clone https://github.com/IllDepence/Canvas-Indexer.git
 cp -v jk/.dockerignore jk/Dockerfile jk/gunicorn_config.py jk/config.ini JSONkeeper
 cp -v ci/.dockerignore ci/Dockerfile ci/gunicorn_config.py ci/config.ini ci/log.txt Canvas-Indexer
+sed -i -E "s/server_url =.+/server_url = $exturlesc\/curation/" JSONkeeper/config.ini
+sed -i -E "s/as_sources =.+/as_sources = $exturlesc\/curation\/as\/collection.json/" Canvas-Indexer/config.ini
 
 # CV and CF
 rm -rf IIIFCurationViewer
 rm -rf IIIFCurationFinder
 cp -r cv/IIIFCurationViewer .
 cp -r cf/IIIFCurationFinder .
-sed -i -E "s/curationJsonExportUrl: '.+'/curationJsonExportUrl: 'http:\/\/127.0.0.1:8001\/api'/" IIIFCurationViewer/index.js
-sed -i -E "s/curationJsonExportUrl: '.+'/curationJsonExportUrl: 'http:\/\/127.0.0.1:8001\/api'/" IIIFCurationFinder/index.js
-sed -i -E "s/curationViewerUrl: '.+'/curationViewerUrl: 'http:\/\/127.0.0.1:8081'/" IIIFCurationFinder/index.js
-sed -i -E "s/searchEndpointUrl: '.+'/searchEndpointUrl: 'http:\/\/127.0.0.1:8002\/api'/" IIIFCurationFinder/index.js
-sed -i -E "s/facetsEndpointUrl: '.+'/facetsEndpointUrl: 'http:\/\/127.0.0.1:8002\/facets'/" IIIFCurationFinder/index.js
-sed -i -E "s/redirectUrl: '.+'/redirectUrl: 'http:\/\/127.0.0.1:8081'/" IIIFCurationFinder/exportJsonKeeper.js
 cp -v cv/.dockerignore cv/Dockerfile IIIFCurationViewer
 cp -v cf/.dockerignore cf/Dockerfile IIIFCurationFinder
+sed -i -E "s/curationJsonExportUrl: '.+'/curationJsonExportUrl: '$exturlesc\/curation\/api'/" IIIFCurationViewer/index.js
+sed -i -E "s/curationJsonExportUrl: '.+'/curationJsonExportUrl: '$exturlesc\/curation\/api'/" IIIFCurationFinder/index.js
+sed -i -E "s/curationViewerUrl: '.+'/curationViewerUrl: '$exturlesc\/view\/'/" IIIFCurationFinder/index.js
+sed -i -E "s/searchEndpointUrl: '.+'/searchEndpointUrl: '$exturlesc\/ci\/api'/" IIIFCurationFinder/index.js
+sed -i -E "s/facetsEndpointUrl: '.+'/facetsEndpointUrl: '$exturlesc\/ci\/facets'/" IIIFCurationFinder/index.js
+sed -i -E "s/redirectUrl: '.+'/redirectUrl: '$exturlesc\/view\/'/" IIIFCurationFinder/exportJsonKeeper.js
 
 ./reset.sh
