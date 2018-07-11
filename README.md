d34549c3151731c102c725ee7a814e526395028e
Tarek Saier <tareksaier@gmail.com>
2018-07-11 14:44:13 +0900

    retrieve IIIF Curation Viewer and Finder from web

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Docker setup for running all parts of the [IIIF Curation Platform](http://codh.r
 * *if* Firebase is to be used
     * place a file `firebase-adminsdk.json` in jk/
     * uncomment the firebase config section in jk/config.ini
-    * configure the IIIF Curation Viewer and Finder's authFirebase.js accordingly
+    * place a modified `authFirebase.js` in cv/ and cf/
     * add the host part of `externalurl` to the authorized domains in your Firebase console
 * `$ ./setup.sh`
 * `$ docker-compose up --build`
--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 # Configurate the external URL here (without trailing slash)
 externalurl=http://136.187.82.133/cp
 
+
+jk_git_url="https://github.com/IllDepence/JSONkeeper.git"
+ci_git_url="https://github.com/IllDepence/Canvas-Indexer.git"
+cv_zip_url="http://codh.rois.ac.jp/software/download/IIIFCurationViewer_20180710.zip"
+cf_zip_url="http://codh.rois.ac.jp/software/download/IIIFCurationFinder_20180710.zip"
+
 # NOTE: services are expected to be accessible at
 # - JSONkeeper: <externalurl>/curation/
 # - Canvas Indexer: <externalurl>/ci/
@@ -21,8 +27,8 @@ exturlesc="${externalurl//\//\\/}"
 # Jk and CI
 rm -rf JSONkeeper
 rm -rf Canvas-Indexer
-git clone https://github.com/IllDepence/JSONkeeper.git
-git clone https://github.com/IllDepence/Canvas-Indexer.git
+git clone $jk_git_url
+git clone $ci_git_url
 cp -v jk/.dockerignore jk/* JSONkeeper
 cp -v ci/.dockerignore ci/* Canvas-Indexer
 sed -i -E "s/server_url =.+/server_url = $exturlesc\/curation/" JSONkeeper/config.ini
@@ -31,10 +37,25 @@ sed -i -E "s/as_sources =.+/as_sources = $exturlesc\/curation\/as\/collection.js
 # CV and CF
 rm -rf IIIFCurationViewer
 rm -rf IIIFCurationFinder
-cp -r cv/IIIFCurationViewer .
-cp -r cf/IIIFCurationFinder .
-cp -v cv/.dockerignore cv/Dockerfile IIIFCurationViewer
-cp -v cf/.dockerignore cf/Dockerfile IIIFCurationFinder
+
+curl $cv_zip_url -o cv_tmp.zip
+mkdir cv_tmp_folder
+unzip -q -d cv_tmp_folder cv_tmp.zip
+rm cv_tmp.zip
+cvunzipped=`ls cv_tmp_folder`
+mv -v "cv_tmp_folder/${cvunzipped}" IIIFCurationViewer
+rmdir cv_tmp_folder
+
+curl $cf_zip_url -o cf_tmp.zip
+mkdir cf_tmp_folder
+unzip -q -d cf_tmp_folder cf_tmp.zip
+rm cf_tmp.zip
+cfunzipped=`ls cf_tmp_folder`
+mv -v "cf_tmp_folder/${cfunzipped}" IIIFCurationFinder
+rmdir cf_tmp_folder
+
+cp -v cv/.dockerignore cv/* IIIFCurationViewer
+cp -v cf/.dockerignore cf/* IIIFCurationFinder
 sed -i -E "s/curationJsonExportUrl: '.+'/curationJsonExportUrl: '$exturlesc\/curation\/api'/" IIIFCurationViewer/index.js
 sed -i -E "s/curationJsonExportUrl: '.+'/curationJsonExportUrl: '$exturlesc\/curation\/api'/" IIIFCurationFinder/index.js
 sed -i -E "s/curationViewerUrl: '.+'/curationViewerUrl: '$exturlesc\/viewer\/'/" IIIFCurationFinder/index.js
